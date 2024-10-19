0fa53200da30e286e26e6bc3cc67066f649b7c19
guelaoismael <guelaoismael@gmail.com>
2024-10-19 11:44:31 +0100

    Add script that inserts a single row in the table customer

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+customer_id = 1;
+customer_name = 'Cole Baidoo';
+email = 'cbaidoo@sandtech.com';
+address = '123 Happiness Ave.';
+
+INSERT INTO Books(customer_id, customer_name, email, address) VALUES (customer_id, customer_name, email, address);
